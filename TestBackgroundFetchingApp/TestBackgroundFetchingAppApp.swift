46cd43498b1f8338ffb3abc9719555ee9cf6f7f8
Areg Vardanian <aregv@bk.ru>
2022-02-07 18:11:36 +0300

    Initial Commit

---
+++ TestBackgroundFetchingApp/TestBackgroundFetchingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBackgroundFetchingAppApp.swift
+//  TestBackgroundFetchingApp
+//
+//  Created by Геворг on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestBackgroundFetchingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
